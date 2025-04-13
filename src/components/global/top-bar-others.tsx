974ffcc455db78defa977e3212f88516de187404
leanneyeong <leanneyeong@gmail.com>
2025-04-13 23:34:42 +0800

    added user button to top-bar

--- src/components/global/top-bar-others.tsx
+++ src/components/global/top-bar-others.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-
+import { UserButton } from "@clerk/nextjs";
 
 // TopBar Component
 export const TopBar: React.FC = () => {
   return (
     <header className="bg-blue-500 text-white p-4">
-      <div className="container mx-auto">
+      <div className="container mx-auto flex justify-between items-center">
         <h1 className="text-2xl font-bold">Parking made Smarter</h1>
+        <div className="ml-auto">
+          <UserButton />
+        </div>
       </div>
     </header>
   );
